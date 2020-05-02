MCHSL/extools

commit 740e6e581582d8e6f7bf6d33b4916bc7c69a1e00

Linux stuff (#24)
* Progress!

* Most of the functions

* Make it compile on Linux

* loonix

* Reee!

* Make all those warnings

* Ensure stuff that won't work on Linux doesn't actually initialize.

* Fix GCC errors

* Sockets now work on Linux.

byond-extools/src/debug_server/debug_server.cpp
@@ -575,7 +575,7 @@ __declspec(naked) void singlestep_hook()
 	}
 }
 #else
-extern "C" void singlestep_hook();
+void singlestep_hook() {};
 #endif
 
 /*
@@ -594,8 +594,10 @@ void install_singlestep_hook()
 {
 	char* opcode_switch = (char*)Pocket::Sigscan::FindPattern("byondcore.dll", "0F B7 48 14 8B 78 10 8B F1 8B 14 B7 81 FA");
 	std::uint32_t addr = (std::uint32_t) & singlestep_hook;
+#ifdef _WIN32
 	DWORD old_prot;
 	VirtualProtect((void*)opcode_switch, 16, PAGE_EXECUTE_READWRITE, &old_prot);
+#endif
 	opcode_switch[0] = 0xBA; //MOV EDX,
 	opcode_switch[1] = nth(addr, 0);
 	opcode_switch[2] = nth(addr, 1);
@@ -603,11 +605,14 @@ void install_singlestep_hook()
 	opcode_switch[4] = nth(addr, 3); //address of singlestep_hook
 	opcode_switch[5] = 0xFF; //CALL
 	opcode_switch[6] = 0xD2; //EDX
+#ifdef _WIN32
 	VirtualProtect((void*)opcode_switch, 16, old_prot, &old_prot);
+#endif
 }
 
 bool debugger_initialize()
 {
+#ifdef _WIN32
 	static bool debugger_initialized = false;
 	if (debugger_initialized)
 	{
@@ -620,6 +625,10 @@ bool debugger_initialize()
 	nop_opcode = Core::register_opcode("DEBUG_NOP", on_nop);
 	debugger_initialized = true;
 	return true;
+#else
+	Core::alert_dd("The extools debugger is not supported on Linux.");
+	return false;
+#endif
 }
 
 bool debugger_enable(const char* mode, const char* port)

byond-extools/CMakeLists.txt
@@ -6,17 +6,27 @@ else()
 endif()
 
 project(byond-extools)
-enable_language(ASM)
+if (WIN32)
+    enable_language(ASM)
+endif()
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 set(SRC_DIR ${CMAKE_SOURCE_DIR}/src)
-file(GLOB_RECURSE SRC_FILES
-    "${SRC_DIR}/*.cpp"
-    "${SRC_DIR}/*.h"
-    "${SRC_DIR}/*.hpp"
-    "${SRC_DIR}/*.s"
-)
+if (WIN32)
+    file(GLOB_RECURSE SRC_FILES
+        "${SRC_DIR}/*.cpp"
+        "${SRC_DIR}/*.h"
+        "${SRC_DIR}/*.hpp"
+        "${SRC_DIR}/*.s"
+    )
+else()
+    file(GLOB_RECURSE SRC_FILES
+        "${SRC_DIR}/*.cpp"
+        "${SRC_DIR}/*.h"
+        "${SRC_DIR}/*.hpp"
+    )
+endif()
 set(SRC_FILES ${SRC_FILES} "${SRC_DIR}/core/subhook/subhook.c")
 
 add_library(byond-extools SHARED ${SRC_FILES})

byond-extools/src/core/byond_structures.cpp
@@ -162,7 +162,7 @@ Value& Value::operator/=(const Value& rhs)
 	return *this;
 }
 
-#define VALOPS(ret, l, r, op) inline ret operator##op##(l lhs, r rhs) { return lhs ##op##= rhs; }
+#define VALOPS(ret, l, r, op) inline ret operator op(l lhs, r rhs) { return lhs op##= rhs; }
 #define ALLVALOPS(ret, l, r) VALOPS(ret, l, r, +); VALOPS(ret, l, r, -); VALOPS(ret, l, r, *); VALOPS(ret, l, r, /);
 
 ALLVALOPS(Value, Value, Value&)

byond-extools/src/core/core.cpp
@@ -115,7 +115,7 @@ void Core::Alert(std::string what) {
 #ifdef _WIN32
 	MessageBoxA(NULL, what.c_str(), "Ouch!", MB_OK);
 #else
-	printf("%s\n", what);
+	printf("Ouch!: %s\n", what.c_str());
 #endif
 }
 
@@ -394,7 +394,11 @@ Value Core::global_direct_get(std::string name)
 
 void Core::disconnect_client(unsigned int id)
 {
+#ifdef _WIN32
 	closesocket(get_socket_from_client(id));
+#else
+	close(get_socket_from_client(id));
+#endif
 	DisconnectClient1(id, 1, 1);
 	DisconnectClient2(id);
 }

byond-extools/src/core/find_functions.cpp
@@ -15,8 +15,13 @@
 
 bool Core::verify_compat()
 {
+#ifdef _WIN32
 	IMPORT_OR_DIE(GetByondVersion, "?GetByondVersion@ByondLib@@QAEJXZ");
 	IMPORT_OR_DIE(GetByondBuild, "?GetByondBuild@ByondLib@@QAEJXZ");
+#else
+	IMPORT_OR_DIE(GetByondVersion, "_ZN8ByondLib15GetByondVersionEv");
+	IMPORT_OR_DIE(GetByondBuild, "_ZN8ByondLib13GetByondBuildEv");
+#endif
 	ByondVersion = GetByondVersion();
 	ByondBuild = GetByondBuild();
 	if (ByondVersion < MIN_COMPATIBLE_MAJOR || ByondBuild < MIN_COMPATIBLE_MINOR)
@@ -101,8 +106,33 @@ bool Core::find_functions()
 	FIND_OR_DIE(CallGlobalProc, "55 89 E5 81 EC ?? ?? ?? ?? 89 5D ?? 89 C3 0F B6 45");
 	FIND_OR_DIE(ProcCleanup, "55 89 E5 56 89 C6 53 83 EC ?? 8B 58 ??");
 	FIND_OR_DIE(CreateContext, "55 89 E5 57 56 89 C6 53 89 D3 83 EC ?? 80 3D ?? ?? ?? ?? ??");
-#error GetTypeById missing
-#error MobTableIndexToGlobalTableIndex missing
+	FIND_OR_DIE(Runtime, "55 89 E5 57 56 53 83 EC ?? A1 ?? ?? ?? ?? 85 C0 0F 84 ?? ?? ?? ?? 0F B6 48 ??");
+	FIND_OR_DIE(DisconnectClient2, "55 89 E5 57 56 53 83 EC ?? 0F B7 7D ?? 66 3B 3D ?? ?? ?? ??");
+	FIND_OR_DIE(CreateList, "55 89 E5 57 56 53 83 EC ?? A1 ?? ?? ?? ?? 8B 75 ?? 85 C0 0F 84 ?? ?? ?? ??");
+	FIND_OR_DIE(SendMaps, "55 89 E5 57 56 53 81 EC ?? ?? ?? ?? 80 3D ?? ?? ?? ?? ?? 0F 84 ?? ?? ?? ??");
+	FIND_OR_DIE(ToString, "55 89 E5 83 EC ?? 89 5D ?? 8B 5D ?? 89 75 ?? 8B 75 ?? 89 7D ?? 80 FB ??");
+	FIND_OR_DIE(Length, "55 89 E5 57 56 53 83 EC ?? 8B 45 ?? 8B 5D ?? 3C ??");
+	FIND_OR_DIE(PrintToDD, "55 89 E5 53 83 EC ?? 8B 1D ?? ?? ?? ?? 8B 55 ?? 85 DB");
+	FIND_OR_DIE(TopicFloodCheck, "55 89 E5 57 56 53 83 EC ?? 0F B7 45 ?? 89 04 24 E8 ?? ?? ?? ?? BA ?? ?? ?? ??");
+	FIND_OR_DIE(GetTurf, "55 89 E5 83 EC ?? 0F BF 0D ?? ?? ?? ??");
+	FIND_OR_DIE(GetTypeById, "55 31 C0 89 E5 8B 55 ?? 39 15 ?? ?? ?? ?? 76 ?? 6B C2 ??");
+	FIND_OR_DIE(MobTableIndexToGlobalTableIndex, "55 31 C0 89 E5 8B 55 ?? 39 15 ?? ?? ?? ?? 76 ?? 89 D0");
+	FIND_OR_DIE(AppendToContainer, "55 89 E5 83 EC ?? 3C ?? 89 5D ?? 8B 5D ?? 89 75 ?? 8B 75 ?? 89 7D ?? 76 ??");
+	FIND_OR_DIE(DisconnectClient1, "55 89 E5 81 EC ?? ?? ?? ?? 89 75 ?? 8D B5 ?? ?? ?? ?? 89 5D ?? 89 D3");
+	FIND_OR_DIE(GetListPointerById, "55 89 E5 83 EC ?? 89 04 24 E8 ?? ?? ?? ??");
+	FIND_OR_DIE(SetAssocElement, "55 B9 ?? ?? ?? ?? 89 E5 83 EC ?? 89 75 ??");
+	FIND_OR_DIE(CallProcByName, "55 89 E5 83 EC ?? 89 5D ?? 89 D3 8B 55 ?? 89 45 ?? 0F B6 45 ?? 89 75 ?? 89 CE 8B 4D ?? 89 7D ?? 89 14 24 8B 55 ?? 88 45 ?? 0F B6 F8 8D 45 ?? 89 44 24 ?? 89 F8 89 4C 24 ?? 31 C9 C6 45 ?? ?? C7 44 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? 80 7D ?? ?? 0F 84 ?? ?? ?? ?? 3D ?? ?? ?? ?? 74 ?? 8B 4D ?? 8B 55 ?? 89 44 24 ?? 8D 45 ?? C7 44 24 ?? ?? ?? ?? ?? 89 4C 24 ?? 8B 4D ?? 89 54 24 ?? 8B 55 ?? 89 3C 24 89 4C 24 ?? 89 F1 89 54 24 ?? 89 DA E8 ?? ?? ?? ?? 8B 45 ?? 8B 55 ?? 8B 4D ?? 89 01 89 51 ?? 8D B6 ?? ?? ?? ?? 8B 45 ?? 8B 5D ?? 8B 75 ?? 8B 7D ?? 89 EC 5D C3 F7 C7 ?? ?? ?? ?? 74 ?? 80 7D ?? ?? 0F 84 ?? ?? ?? ?? 8B 4D ?? C7 01 ?? ?? ?? ?? C7 41 ?? ?? ?? ?? ?? 8B 45 ?? 8B 55 ?? 89 44 24 ?? 89 14 24 E8 ?? ?? ?? ?? 80 7D ?? ?? 74 ?? C7 04 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? C7 44 24 ?? ?? ?? ?? ?? C7 04 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8B 45 ?? 8B 55 ?? C7 44 24 ?? ?? ?? ?? ?? C7 44 24 ?? ?? ?? ?? ?? 89 04 24 89 54 24 ?? E8 ?? ?? ?? ?? 8B 55 ?? 89 14 24 E8 ?? ?? ?? ?? 8B 18 C7 44 24 ?? ?? ?? ?? ?? C7 04 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? 89 5C 24 ?? 89 04 24 E8 ?? ?? ?? ?? C7 44 24 ?? ?? ?? ?? ?? 89 04 24 E8 ?? ?? ?? ?? C7 04 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? A1 ?? ?? ?? ?? 89 04 24 E8 ?? ?? ?? ??"); // OH DEAR GOD
+#warning TODO: IsInContainer
+#warning TODO: GetSocketHandleStruct
+#warning TODO: GetAssocElement
+#warning TODO: GetGlobalByName
+#warning TODO: global_var_table
+#warning TODO: IncRefCount
+#warning TODO: DecRefCount
+#warning TODO: x_ref_count_call
+#warning TODO: GetTableHolderThingyById
+#warning TODO: GetBSocket
+#warning TODO: name_table
 	switch(ByondVersion) {
 		case 512:
 			FIND_OR_DIE(SetVariable, "55 89 E5 81 EC ?? ?? ?? ?? 89 75 ?? 8B 55 ?? 8B 75 ??");
@@ -111,13 +141,13 @@ bool Core::find_functions()
 		case 513:
 			FIND_OR_DIE(SetVariable, "55 89 E5 81 EC ?? ?? ?? ?? 8B 55 ?? 8B 45 ??");
 			FIND_OR_DIE(GetStringTableIndexUTF8, "55 89 E5 57 56 89 CE 53 89 D3 83 EC ?? 8B 55 ?? 85 C0") // regparm3
+			parent_context_ptr_hack = *(ExecutionContext***)Pocket::Sigscan::FindPattern(BYONDCORE, "A1 ?? ?? ?? ?? 8B 4D ?? 0F B7 50", 1);
 			break;
 		default: break;
 	}
 	current_execution_context_ptr = *(ExecutionContext * **)Pocket::Sigscan::FindPattern(BYONDCORE, "A1 ?? ?? ?? ?? 8D 7D ?? 89 78 ??", 1);
 	proc_setup_table = **(ProcSetupEntry * ***)Pocket::Sigscan::FindPattern(BYONDCORE, "A1 ?? ?? ?? ?? 8B 04 98 85 C0 74 ?? 89 04 24 E8 ?? ?? ?? ?? 8B 15 ?? ?? ?? ??", 1);
 	some_flags_including_profile = *(unsigned int**)Pocket::Sigscan::FindPattern(BYONDCORE, "A1 ?? ?? ?? ?? A8 ?? 0F 85 ?? ?? ?? ??", 2);
-	parent_context_ptr_hack = *(ExecutionContext***)Pocket::Sigscan::FindPattern(BYONDCORE, "A1 ?? ?? ?? ?? 8B 4D ?? 0F B7 50 ", 1);
 #endif
 	return true;
 }

byond-extools/src/core/socket/socket.cpp
@@ -8,6 +8,7 @@ static bool InitOnce()
 	}
 	// It doesn't really matter if we call this a couple extra times.
 	// It also doesn't really matter if we never call WSACleanup.
+#ifdef _WIN32
 	WSADATA wsaData;
 	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0)
@@ -16,6 +17,7 @@ static bool InitOnce()
 		return false;
 	}
 	done = true;
+#endif
 	return true;
 }
 
@@ -41,7 +43,11 @@ void Socket::close()
 {
 	if (raw_socket != INVALID_SOCKET)
 	{
+#ifdef _WIN32
 		closesocket(raw_socket);
+#else
+		::close(raw_socket);
+#endif
 		raw_socket = INVALID_SOCKET;
 	}
 }
@@ -65,7 +71,8 @@ bool connect_socket(Socket& socket, const char* port, const char* remote, bool y
 	struct addrinfo hints;
 	int iResult;
 
-	ZeroMemory(&hints, sizeof(hints));
+	memset(&hints, 0, sizeof(hints));
+
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
@@ -82,7 +89,9 @@ bool connect_socket(Socket& socket, const char* port, const char* remote, bool y
 	// Create a SOCKET for connecting to server
 	if (!socket.create(result->ai_family, result->ai_socktype, result->ai_protocol))
 	{
+#ifdef _WIN32
 		if(yell) Core::Alert("socket failed with error: " + std::to_string(WSAGetLastError()));
+#endif
 		freeaddrinfo(result);
 		return false;
 	}
@@ -91,7 +100,9 @@ bool connect_socket(Socket& socket, const char* port, const char* remote, bool y
 	iResult = ::connect(socket.raw(), result->ai_addr, (int)result->ai_addrlen);
 	if (iResult == SOCKET_ERROR)
 	{
+#ifdef _WIN32
 		if(yell) Core::Alert("connect failed with error: " + std::to_string(WSAGetLastError()));
+#endif
 		freeaddrinfo(result);
 		socket.close();
 		return false;
@@ -104,7 +115,7 @@ bool connect_socket(Socket& socket, const char* port, const char* remote, bool y
 bool JsonListener::listen(const char* port, const char* iface)
 {
 	struct addrinfo* result = NULL;
-	struct addrinfo hints;
+	struct addrinfo* hints = (addrinfo*)malloc(sizeof(hints));
 	int iResult;
 
 	// Initialize Winsock
@@ -113,14 +124,13 @@ bool JsonListener::listen(const char* port, const char* iface)
 		return false;
 	}
 
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
+	hints->ai_family = AF_INET;
+	hints->ai_socktype = SOCK_STREAM;
+	hints->ai_protocol = IPPROTO_TCP;
+	hints->ai_flags = AI_PASSIVE;
 
 	// Resolve the server address and port
-	iResult = getaddrinfo(iface, port, &hints, &result);
+	iResult = getaddrinfo(iface, port, hints, &result);
 	if (iResult != 0)
 	{
 		Core::Alert("getaddrinfo failed with error: " + std::to_string(iResult));
@@ -130,7 +140,9 @@ bool JsonListener::listen(const char* port, const char* iface)
 	// Create a SOCKET for connecting to server
 	if (!socket.create(result->ai_family, result->ai_socktype, result->ai_protocol))
 	{
+#ifdef _WIN32
 		Core::Alert("socket failed with error: " + std::to_string(WSAGetLastError()));
+#endif
 		freeaddrinfo(result);
 		return false;
 	}
@@ -144,7 +156,9 @@ bool JsonListener::listen(const char* port, const char* iface)
 	iResult = bind(socket.raw(), result->ai_addr, (int)result->ai_addrlen);
 	if (iResult == SOCKET_ERROR)
 	{
+#ifdef _WIN32
 		Core::Alert("bind failed with error: " + std::to_string(WSAGetLastError()));
+#endif
 		freeaddrinfo(result);
 		socket.close();
 		return false;
@@ -155,7 +169,9 @@ bool JsonListener::listen(const char* port, const char* iface)
 	iResult = ::listen(socket.raw(), SOMAXCONN);
 	if (iResult == SOCKET_ERROR)
 	{
+#ifdef _WIN32
 		Core::Alert("listen failed with error: " + std::to_string(WSAGetLastError()));
+#endif
 		socket.close();
 		return false;
 	}

byond-extools/src/core/socket/socket.h
@@ -9,6 +9,15 @@
 #include <ws2tcpip.h>
 #include <windows.h>
 #pragma comment (lib, "Ws2_32")
+#else
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
+#include <unistd.h> /* Needed for close() */
+#define SOCKET int
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
 #endif
 
 const char* const DBG_DEFAULT_PORT = "2448";

byond-extools/src/core/various_testing.cpp
@@ -81,6 +81,7 @@ public:
 	}
 };
 
+#ifdef _WIN32
 opcode_handler _generate_number_op_localm_localn_store_localx(unsigned char op, int localm, int localn, int localx)
 {
 	unsigned char* func = (unsigned char*)VirtualAlloc(NULL, 64, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
@@ -97,8 +98,9 @@ opcode_handler _generate_number_op_localm_localn_store_localx(unsigned char op, 
 	VirtualProtect(func, 64, PAGE_EXECUTE_READ, &old_prot);
 	return (opcode_handler)func;
 }
+#endif
 
-extern "C" __declspec(dllexport) void add_subvars_of_locals(ExecutionContext* ctx)
+extern "C" EXPORT void add_subvars_of_locals(ExecutionContext* ctx)
 {
 	Value a = ctx->local_variables[0];
 	Value b = ctx->local_variables[1];

commit 0e314341d53e138d1572195964b08a71c7a6285a

Fix single-step hook on cross-compile builds
Broken in 740e6e581582d8e6f7bf6d33b4916bc7c69a1e00.

byond-extools/src/debug_server/debug_server.cpp
@@ -582,7 +582,7 @@ __declspec(naked) void singlestep_hook()
 	}
 }
 #else
-void singlestep_hook() {};
+extern "C" void singlestep_hook();
 #endif
 
 /*