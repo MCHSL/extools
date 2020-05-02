0e07a873c261adc98352973a689028d29318ce75
Tad Hardesty <tad@platymuus.com>
2020-05-01 22:30:30 -0700

    Move extended profiling exports to their own file

--- byond-extools/src/core/core.cpp
+++ byond-extools/src/core/core.cpp
@@ -244,27 +244,6 @@ extern "C" EXPORT const char* run_tests(int n_args, const char** args)
 	return good;
 }
 
-// TODO: make this work on Linux. -steamport
-extern "C" EXPORT const char* extended_profiling_initialize(int n_args, const char** args)
-{
-	if (!(Core::initialize() && actual_extended_profiling_initialize()))
-		return bad;
-	return good;
-}
-
-extern "C" EXPORT const char* enable_extended_profiling(int n_args, const char** args)
-{
-	//Core::Alert("Enabling logging for " + std::string(args[0]));
-	Core::get_proc(args[0]).extended_profile();
-	return good;
-}
-
-extern "C" EXPORT const char* disable_extended_profiling(int n_args, const char** args)
-{
-	procs_to_profile.erase(Core::get_proc(args[0]).id); //TODO: improve consistency and reconsider how initialization works
-	return good;
-}
-
 std::uint32_t Core::get_socket_from_client(unsigned int id)
 {
 	int str = (int)GetSocketHandleStruct(id);
