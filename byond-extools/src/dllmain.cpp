84bdbf635defa458a30b620064a487fac5916a82
Tad Hardesty <tad@platymuus.com>
2020-05-01 20:50:04 -0700

    Tidy dllmain.cpp

--- byond-extools/src/dllmain.cpp
+++ byond-extools/src/dllmain.cpp
@@ -3,21 +3,18 @@
 #include <windows.h>
 #include "core/core.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
 		break;
-    case DLL_PROCESS_DETACH:
+	case DLL_PROCESS_DETACH:
 		Core::cleanup();
-        break;
-    }
-    return TRUE;
+		break;
+	}
+	return TRUE;
 }
 #endif
