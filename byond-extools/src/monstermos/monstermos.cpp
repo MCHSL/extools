7daba28211e6b0341194f265e4dfa4215ee1bed3
Maxim Nikitin <mnmaxim@gmail.com>
2020-11-12 18:40:33 -0500

    Update monstermos.cpp

--- byond-extools/src/monstermos/monstermos.cpp
+++ byond-extools/src/monstermos/monstermos.cpp
@@ -181,11 +181,11 @@ trvh gasmixture_set_temperature(unsigned int args_len, Value* args, Value src)
 {
 	float vf = args_len > 0 ? args[0].valuef : 0;
 	if (std::isnan(vf) || std::isinf(vf)) {
+		get_gas_mixture(src)->set_temperature(0);
 		Runtime("Attempt to set temperature to NaN or Infinity");
-		get_gas_mixture(src)->set_temperature(0);
 	} else if(vf < 0) {
-		Runtime("Attempt to set temperature to negative number");
 		get_gas_mixture(src)->set_temperature(0);
+		Runtime("Attempt to set temperature to negative number");
 	} else {
 		get_gas_mixture(src)->set_temperature(vf);
 	}
@@ -213,11 +213,11 @@ trvh gasmixture_set_moles(unsigned int args_len, Value* args, Value src)
 	int index = gas_ids[args[0].value];
 	float vf = args[1].valuef;
 	if (std::isnan(vf) || std::isinf(vf)) {
+		get_gas_mixture(src)->set_moles(index, 0);
 		Runtime("Attempt to set moles to NaN or Infinity");
-		get_gas_mixture(src)->set_moles(index, 0);
 	} else if(vf < 0) {
-		Runtime("Attempt to set moles to negative number");
 		get_gas_mixture(src)->set_moles(index, 0);
+		Runtime("Attempt to set moles to negative number");
 	} else {
 		get_gas_mixture(src)->set_moles(index, vf);
 	}
